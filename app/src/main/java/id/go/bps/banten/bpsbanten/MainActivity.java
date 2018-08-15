f14c4213ed9d790099e49aaa3759cab34aaf5f57
Muhammad Irfan Fadilah <mirfanfad@gmail.com>
2018-08-15 11:31:14 +0700

    new

--- app/src/main/java/id/go/bps/banten/bpsbanten/MainActivity.java
+++ app/src/main/java/id/go/bps/banten/bpsbanten/MainActivity.java
@@ -71,13 +71,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_brs) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_isp) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_publikasi) {
 
         } else if (id == R.id.nav_share) {
 
